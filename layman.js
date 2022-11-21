d9fa6510c20eae7e9e90a2f866859327de2568d6
addiment <63932310+addiment@users.noreply.github.com>
2022-11-21 14:28:04 -0500

    how to make simple? remove comment!

--- layman.js
+++ layman.js
@@ -5,12 +5,8 @@ const net = require('node:net');
 // For user input
 const readline = require('node:readline');
 
-// Configuration file
-const cfg = require('./cfg.json');
-
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms = 1000));
-}
+const CLIENT_ADDRESS = 'localhost';
+const PORT = 2023;
 
 /** @typedef {'system'|'incoming'|'outgoing'} MessageType */
 
@@ -20,20 +16,14 @@ async function sleep(ms) {
  * @property {MessageType} type Whether the message is something we sent, something we received, or something like an error message.
 */
 
-/**
- * Array of all past messages.
- * @type {MessageStruct[]}
- */
+/** Array of all past messages.
+ * @type {MessageStruct[]} */
 var messageHistory = [];
-/**
- * The socket messages are sent through 
- * @type {net.Socket}
- */
+/** The socket messages are sent through 
+ * @type {net.Socket} */
 var messageSocket = null;
-/**
- * The server (if hosting).
- * @type {net.Server}
- */
+/** The server (if hosting).
+ * @type {net.Server} */
 var chatServer = null;
 
 const rl = readline.createInterface({
@@ -42,20 +32,14 @@ const rl = readline.createInterface({
     prompt: '',
 });
 
-// Regenerates and prints the prompt.
 function regenPrompt() {
-    // Creates a bar (string) the width of the screen
     let out = '';
     for (var i = 0; i < process.stdout.columns; i++) {
-        out += '\u2588'; // The Unicode "full block" character, U+2588
+        out += '\u2588';
     }
-    // Make a new line
     out += '\n';
-    // Write the bar to the screen
     process.stdout.write(out);
-    // Move the cursor back to where it was (the text input)
     process.stdout.cursorTo(rl.getCursorPos().cols, process.stdout.rows);
-    // Regenerate the readline module prompt
     rl.prompt(true);
 }
 
@@ -65,10 +49,10 @@ function fullRedraw() {
     process.stdout.write('\x1b[2J\x1b[3J');
     process.stdout.cursorTo(0, process.stdout.rows); // Move the cursor to the bottom to start
     messageHistory.forEach(msg => {
-        pushMessage(msg.content, msg.type, true); // Print old messages
+        pushMessage(msg.content, msg.type, true);
     });
     process.stdout.write('\n');
-    regenPrompt(); // Generate a prompt
+    regenPrompt();
 }
 
 /**
@@ -78,7 +62,6 @@ function fullRedraw() {
  */
 function printWithDirectionWrap(str, side = 'left', indicator = '') {
     let splitStrings = [];
-    // process.stdout.columns = good for max
     let maxLineLength = Math.floor(process.stdout.columns / 2);
     let splitCount = Math.ceil((str.length + indicator.length) / maxLineLength);
     let lineOffset = 0;
@@ -105,11 +88,11 @@ function printWithDirectionWrap(str, side = 'left', indicator = '') {
  * @param {boolean} [isRedraw] Whether or not to move the cursor around willy-nilly and add message to history. This is used for the {@linkcode fullRedraw} function.
  */
 function pushMessage(str, type = 'system', isRedraw) {
-    if (!isRedraw) process.stdout.moveCursor(0, -2); // Move cursor up twice (above the prompt bar)
-    process.stdout.cursorTo(0); // Move the cursor all the way to the left
-    process.stdout.clearScreenDown(); // Clear the old prompt, do this cuz the command line overwrites old text instead of inserting it
-    if (!isRedraw) messageHistory.push({ content: str, type }); // Add to message history
-    switch (type) { // Change the style based on who sent the message
+    if (!isRedraw) process.stdout.moveCursor(0, -2);
+    process.stdout.cursorTo(0);
+    process.stdout.clearScreenDown();
+    if (!isRedraw) messageHistory.push({ content: str, type });
+    switch (type) {
         case 'system':
             printWithDirectionWrap(str, 'left', '');
             break;
@@ -122,23 +105,21 @@ function pushMessage(str, type = 'system', isRedraw) {
         default:
             throw new Error(`"${type}" is not a valid message type!`);
     }
-    process.stdout.cursorTo(0); // Send the cursor back to the horizontal beginning again
+    process.stdout.cursorTo(0);
     if (!isRedraw) {
-        process.stdout.moveCursor(0, 1); // Move the cursor up (just as a spacer)
-        regenPrompt(); // Make a new prompt
+        process.stdout.moveCursor(0, 1);
+        regenPrompt();
     }
     return;
 }
 
 /**
  * Quits the program and closes any open connections
- * @param {boolean} [error] An error, if any.
+ * @param {boolean} [error] If there was an error
  */
 function quit(error) {
     closeSocket();
-    // Close readline
     rl.close();
-    // Exit gracefully
     process.exitCode = !!error;
 }
 
@@ -171,22 +152,17 @@ function handleSocket(socket, isReady) {
     });
     if (isReady) socket.emit('connect', false);
 
-    // When the socket closes
     socket.on('close', didErr => {
         closeSocket();
         pushMessage('Connection closed.', 'system');
         quit(didErr);
     });
 
-    // new message
     socket.on('data', data => {
-        // Add to screen
         pushMessage(data.toString('utf-8'), 'incoming');
     });
 }
 
-// When we hit Ctrl+C (aka. SIGINT), close the connection.
-// The program will close on its own when the close event is emitted.
 rl.once('SIGINT', () => {
     if (messageSocket) {
         pushMessage('Issued close.', 'system');
@@ -198,49 +174,36 @@ rl.once('SIGINT', () => {
     }
 });
 
-// When we press enter, do stuff
 rl.on('line', line => {
-    // Offset the newline that gets added to the screen when we press enter
     process.stdout.moveCursor(0, -1);
     if (messageSocket != null && line.trim().length > 0) {
-        // Send our message over the 'net
         messageSocket.write(line);
-        // Add to screen
         pushMessage(line, 'outgoing');
     } else {
-        fullRedraw(); // Whitespace is broken, redraw the screen.
+        fullRedraw();
     }
 });
 
-// Initialize the client
 rl.resume();
-
-// Redraw the window if it changes size
 process.stdout.on('resize', fullRedraw);
-
-// Redraw the screen
 fullRedraw();
 
 if (process.argv.includes('server')) {
     pushMessage("Waiting for a connection...", 'system');
-    // Create a server
     const server = net.createServer();
     chatServer = server;
-    // Limit the server to one connection
     server.maxConnections = 1;
     server.once('connection', socket => {
         handleSocket(socket, true);
         closeServer();
     });
-    // Start server on port 2023
     server.listen(2023);
 } else {
-    // Create connection
     pushMessage("Joining server...", 'system')
     handleSocket(
         net.createConnection({
-            host: cfg.clientAddress,
-            port: 2023
+            host: CLIENT_ADDRESS,
+            port: PORT
         })
     );
 }
